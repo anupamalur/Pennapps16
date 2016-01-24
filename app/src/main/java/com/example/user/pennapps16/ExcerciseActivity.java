381458c45423363aab3fcb93553534642726a251
Prerna Srivastava <Prerna Srivastava>
2016-01-24 04:23:28 -0500

    Added finer grained changes

---
+++ app/src/main/java/com/example/user/pennapps16/ExcerciseActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.pennapps16;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ExcerciseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excercise);
+    }
+}
